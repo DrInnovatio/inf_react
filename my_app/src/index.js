5c245560d5d8188fdfc745e658a90e92e2ec309d
DrInnovatio <turing1723@gmail.com>
2022-06-11 17:47:23 +0900

    presenting the component.

--- my_app/src/index.js
+++ my_app/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 import reportWebVitals from './reportWebVitals';
 //import Library from './chapter03/Library';
-import Clock from './chapter04/Clock';
+//import Clock from './chapter04/Clock';
+import CommentList from './chapter05/CommentList';
 
 //const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
@@ -14,16 +15,19 @@ import Clock from './chapter04/Clock';
 //   </React.StrictMode>
 // );
 
-setInterval(() => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Clock/>
-    </React.StrictMode>,document.getElementById('root')
-  );
-}, 1000);
+// setInterval(() => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <Clock/>
+//     </React.StrictMode>,document.getElementById('root')
+//   );
+// }, 1000);
 
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
