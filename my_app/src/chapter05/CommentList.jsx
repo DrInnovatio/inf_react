dbed57c031cfd8edc7b963ff6315c7bfcda8f0fd
DrInnovatio <turing1723@gmail.com>
2022-06-14 18:48:22 +0900

    using more Comment components.

--- my_app/src/chapter05/CommentList.jsx
+++ my_app/src/chapter05/CommentList.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Comment from './Comment'
 
+const comments = [
+  {
+    name: "James Dean",
+    comment: "I am a movie star."
+  },
+  {
+    name: "Messi",
+    comment: "I am a king of soccer."
+  },
+  {
+    name: "Jay-Z",
+    comment: "Hiphop is my life."
+  },
+]
+
 function CommentList(props) {
   return (
     <div>
-      <Comment />
+      { comments.map((comment) => {
+        return (
+          <Comment name={comment.name} comment={comment.comment}/>
+        )
+      })}
+
     </div>
   );
 }
