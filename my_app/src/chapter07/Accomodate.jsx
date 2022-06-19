1f76b0747f72d48fb00d16a2e4cb7e53a0f7e505
DrInnovatio <turing1723@gmail.com>
2022-06-19 19:12:34 +0900

    added Accomodate.jsx

--- my_app/src/chapter07/Accomodate.jsx
+++ my_app/src/chapter07/Accomodate.jsx
@@ -1,5 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import useCounter from './useCoounter'
 
 const MAX_CAPACITY = 10;
 
+function Accomodate(props) {
+  const [isFull, setIsFull] = useState(false);
+  const [count, increaseCount, decreaseCount] = useCounter(0);
+
+  useEffect(() => {
+    console.log("============================");
+    console.log("useEffect is called.");
+    console.log(`isFull: ${isFull}`);
+  });
+
+  useEffect(() => {
+    setIsFull(count >= MAX_CAPACITY);
+    console.log(`Current count value : ${count}`);
+  }, [count]);
+
+  return (
+    <div style = {{ padding: 16 }}>
+      <p>{ `Total ${count} is in there.` }</p>
+      <button onClick={ increaseCount } disabled={ isFull }>Enter</button>
+      <button onClick={ decreaseCount }>Leaving</button>
+      
+      { isFull && <p style={{ color: "red"}}>It's full.</p>}
+    </div>
+  )
+}
+
+export default Accomodate;
