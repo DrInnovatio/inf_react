5967d8e6f69cecd309d64614609e55aa6e2941cf
DrInnovatio <turing1723@gmail.com>
2022-06-25 15:39:01 +0900

    created Toolbar component.

---
+++ my_app/src/chapter09/Toobar.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const styles = {
+  wrapper: {
+    padding: 16,
+    display: "flex",
+    flexDirection: "row",
+    borderBottom: "1px solid grey",
+  },
+  greeting: {
+    marginRight: 8,
+  }
+};
+function Toolbar(props) {
+  const { isLoggedIn, onClickLogin, onClickLogout } = props;
+
+  return (
+    <div style={ styles.wrapper }>
+      {isLoggedIn && <span style={styles.greeting}>Welcome!!</span>}
+      { isLoggedIn ? (
+        <button onClick={ onClickLogout }> Log Out </button>
+      ) : (
+          <button onClick={ onClickLogin }> Log In </button>
+      )};
+    </div>
+  )
+}
+
+export default Toolbar;
