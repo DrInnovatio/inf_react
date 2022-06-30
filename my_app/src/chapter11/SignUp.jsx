73bf5adb27453f2d47847d2df0a648f974ad01bb
DrInnovatio <turing1723@gmail.com>
2022-06-30 22:03:19 +0900

    done with the Sign up function.

--- my_app/src/chapter11/SignUp.jsx
+++ my_app/src/chapter11/SignUp.jsx
@@ -2,4 +2,25 @@ import React, { useState } from 'react'
 
 function SignUp(props) {
   const [name, setName] = useState("");
+
+  const handleChangeName = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    alert(`NAME: ${name}`);
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={ handleSubmit }>
+      <label>
+        NAME:
+        <input type="text" value={ name } onChange={ handleChangeName } />
+      </label>
+      <button type="submit">SUBMIT</button>
+    </form>
+  );
 }
+
+export default SignUp;
