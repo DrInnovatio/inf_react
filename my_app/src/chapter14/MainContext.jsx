75b1d57cf6dc373b64d5785cc486a7de72386e86
DrInnovatio <turing1723@gmail.com>
2022-07-14 21:51:23 +0900

    context dark theme.

---
+++ my_app/src/chapter14/MainContext.jsx
@@ -0,0 +1,24 @@
+import { useContext } from 'react';
+import ThemeContext from './ThemeContext';
+
+function MainContent(props) {
+
+  const { theme, toggleTheme } = useContext(ThemeContext);
+
+  return (
+    <div
+      style={ {
+        width: "100vw",
+        height: "100vh",
+        padding: "1.5rem",
+        backgroundColor: theme === "light" ? "white" : "black",
+        color: theme === "light" ? "black" : "white",
+      } }
+    >
+      <h1>Changing Theme</h1>
+      <button onClick={ toggleTheme }>Change Theme</button>
+    </div>
+  ); 
+}
+
+export default MainContent;
