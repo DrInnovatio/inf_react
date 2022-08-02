5e1a557ef4548614a75cb51a5e0164bd8eedba7d
DrInnovatio <turing1723@gmail.com>
2022-08-02 20:19:31 +0900

    edited PostViewPage.jsx

--- mini-blog/src/component/page/PostViewPage.jsx
+++ mini-blog/src/component/page/PostViewPage.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import styled from 'styled-components'
+import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import styled from "styled-components";
 import CommentList from "../list/CommentList";
 import TextInput from "../ui/TextInput";
 import Button from "../ui/Button";
@@ -20,78 +20,77 @@ const Container = styled.div`
     max-width: 720px;
 
     & > * {
-      :not(:last-child){
-        margin-bottom: 16px;
-      }
+        :not(:last-child) {
+            margin-bottom: 16px;
+        }
     }
 `;
 
 const PostContainer = styled.div`
-  padding: 8px 16px;
-  border: 1px solid grey;
-  border-radius: 8px;
+    padding: 8px 16px;
+    border: 1px solid grey;
+    border-radius: 8px;
 `;
 
 const TitleText = styled.p`
-  font-size: 28px;
-  font-weight: 500;
+    font-size: 28px;
+    font-weight: 500;
 `;
 
 const ContentText = styled.p`
-  font-size: 20px;
-  line-height: 32px;
-  white-space: pre-wrap;
+    font-size: 20px;
+    line-height: 32px;
+    white-space: pre-wrap;
 `;
 
 const CommentLabel = styled.p`
-  font-size: 16px;
-  font-weight: 500;
+    font-size: 16px;
+    font-weight: 500;
 `;
 
 function PostViewPage(props) {
-  const navigate = useNavigate();
-  const { postId } = useParams();
+    const navigate = useNavigate();
+    const { postId } = useParams();
 
-  const post = data.find((item) => {
-    return item.id = postId;
-  });
+    const post = data.find((item) => {
+        return item.id == postId;
+    });
 
-  const [comment, setComment] = useState("");
+    const [comment, setComment] = useState("");
 
-  return (
-    <Wrapper>
-      <Container>
-        <Button
-          title="backward"
-          onClick={ () => {
-            navigate("/");
-          }}
-        />
-      
-      <postContainer>
-        <TitleText>{ post.title }</TitleText>
-        <ContentText> { post.content }</ContentText>
-      </postContainer>
+    return (
+        <Wrapper>
+            <Container>
+                <Button
+                    title="뒤로 가기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+                <PostContainer>
+                    <TitleText>{post.title}</TitleText>
+                    <ContentText>{post.content}</ContentText>
+                </PostContainer>
 
-      <CommentLabel>Reply</CommentLabel>
-      <CommentList comments={ post.comments } />
-      
-      <TextInput
-        height={ 40 }
-        value={ comment }
-        onChange={ (event) => {
-          setComment(event.target.value);
-        } }
-      />
-      <Button
-        title="Wirte Reply"
-        onClick={ () => {
-          navigate("/");
-        } }
-        />
-        </Container>
-    </Wrapper>
-  )
+                <CommentLabel>댓글</CommentLabel>
+                <CommentList comments={post.comments} />
+
+                <TextInput
+                    height={40}
+                    value={comment}
+                    onChange={(event) => {
+                        setComment(event.target.value);
+                    }}
+                />
+                <Button
+                    title="댓글 작성하기"
+                    onClick={() => {
+                        navigate("/");
+                    }}
+                />
+            </Container>
+        </Wrapper>
+    );
 }
 
 export default PostViewPage;
